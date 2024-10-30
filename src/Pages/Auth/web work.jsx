af4c5b22c6d4ebb617182d27949eebc3459b47b8
unknown <kamichristella@gmail.com>
2024-10-30 19:05:24 +0300

    React and Laravel

---
+++ src/Pages/Auth/web work.jsx
@@ -0,0 +1,41 @@
+<!DOCTYPE html>
+<html>
+<head>
+<style>
+ul {
+  list-style-type: none;
+  margin: 0;
+  padding: 0;
+  width: 200px;
+  background-color: brown;
+}
+
+li a {
+  display: block;
+  color: wheat;
+  padding: 12px;
+  text-decoration: none;
+}
+
+/* Change the link color on hover */
+li a:hover {
+  background-color: brown;
+  color: white;
+}
+</style>
+</head>
+<body>
+
+<h2>Vertical Navigation Bar</h2>
+
+<ul>
+  <li><a href="#home">Home</a></li>
+  <li><a href="#news">Collection</a></li>
+  <li><a href="#contact"><Gallery></a></li>
+  <li><a href="#about">About</a></li>
+</ul>
+
+</body>
+</html>
+
+
